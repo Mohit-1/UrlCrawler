cb93ecfaa5d62f4e390d08feaf8a84868f69833f
Mohit Solanki <mosolank@cisco.com>
2018-12-11 19:10:30 +0530

    Indentation 4 spaces

--- crawler/scraper.py
+++ crawler/scraper.py
@@ -3,57 +3,57 @@ import requests, re
 from requests.exceptions import ConnectionError, MissingSchema
 
 def findIfJQueryExists(url):
-	""" This method makes a request to the received URL and 
-	checks if jQuery exists in the URL's HTML source"""
+    """ This method makes a request to the received URL and 
+    checks if jQuery exists in the URL's HTML source"""
 
-	success, uses_jquery, version, found_in_line = True, -1, None, None
-	data_list = []
+    success, uses_jquery, version, found_in_line = True, -1, None, None
+    data_list = []
 
-	#proxy = {"http": "103.16.70.20:32231"} --- I prefer using proxies while scraping
-	#page_response = requests.get(page_link, timeout=5, proxies=proxy) --- omitting here to reduce vectors for failure
-	try:
-		page_response = requests.get(url, timeout=5)
-	
-	except (ConnectionError, MissingSchema): #Invalid URL or a URL missing the protocol are caught here
-		success, uses_jquery, version, found_in_line = False, -1, None, None
-		data_list.append(success)
-		data_list.append(uses_jquery)
-		data_list.append(version)
-		data_list.append(found_in_line)		
+    #proxy = {"http": "103.16.70.20:32231"} --- I prefer using proxies while scraping
+    #page_response = requests.get(page_link, timeout=5, proxies=proxy) --- omitting here to reduce vectors for failure
+    try:
+        page_response = requests.get(url, timeout=5)
+    
+    except (ConnectionError, MissingSchema): #Invalid URL or a URL missing the protocol are caught here
+        success, uses_jquery, version, found_in_line = False, -1, None, None
+        data_list.append(success)
+        data_list.append(uses_jquery)
+        data_list.append(version)
+        data_list.append(found_in_line)     
 
-		return data_list			
+        return data_list            
 
-	soup = BeautifulSoup(page_response.content, "html.parser")
+    soup = BeautifulSoup(page_response.content, "html.parser")
 
-	for script in soup.findAll('script', {'src': True}):	# Only collect those script tags which have an 'src' attribute
+    for script in soup.findAll('script', {'src': True}):    # Only collect those script tags which have an 'src' attribute
 
-		match = re.search("http[s]{0,1}://ajax\.googleapis\.com/ajax/libs/jquery/([0-9]\.[0-9]+\.[0-9]+)/jquery(\.min){0,1}\.js", script['src'])
-		if match:
-			uses_jquery = 1
-			version = match.group(1)
-			found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')	# Tricky part! Replacing the '<' with '&lt;'
-			break
+        match = re.search("http[s]{0,1}://ajax\.googleapis\.com/ajax/libs/jquery/([0-9]\.[0-9]+\.[0-9]+)/jquery(\.min){0,1}\.js", script['src'])
+        if match:
+            uses_jquery = 1
+            version = match.group(1)
+            found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')  # Tricky part! Replacing the '<' with '&lt;'
+            break
 
-		match = re.search("http[s]{0,1}://code\.jquery\.com/jquery-([0-9]\.[0-9]+\.[0-9]+)(\.min){0,1}\.js", script['src'])
-		if match:
-			uses_jquery = 1
-			version = match.group(1)
-			found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')	# Tricky part! Replacing the '<' with '&lt;'
-			break
+        match = re.search("http[s]{0,1}://code\.jquery\.com/jquery-([0-9]\.[0-9]+\.[0-9]+)(\.min){0,1}\.js", script['src'])
+        if match:
+            uses_jquery = 1
+            version = match.group(1)
+            found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')  # Tricky part! Replacing the '<' with '&lt;'
+            break
 
-		elif script['src'] == "http://code.jquery.com/jquery-latest.min.js" or script['src'] == "https://code.jquery.com/jquery-latest.min.js":
-			uses_jquery = 1
-			found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')
-			break
+        elif script['src'] == "http://code.jquery.com/jquery-latest.min.js" or script['src'] == "https://code.jquery.com/jquery-latest.min.js":
+            uses_jquery = 1
+            found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')
+            break
 
-		elif re.search("jquery(\.min){0,1}\.js", script['src']):
-			uses_jquery = 0
-			found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')
-			break
+        elif re.search("jquery(\.min){0,1}\.js", script['src']):
+            uses_jquery = 0
+            found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')
+            break
 
-	data_list.append(success)
-	data_list.append(uses_jquery)
-	data_list.append(version)
-	data_list.append(found_in_line)		
+    data_list.append(success)
+    data_list.append(uses_jquery)
+    data_list.append(version)
+    data_list.append(found_in_line)     
 
-	return data_list			
+    return data_list            
--- crawler/views.py
+++ crawler/views.py
@@ -5,54 +5,54 @@ from .scraper import findIfJQueryExists
 
 # Create your views here.
 class ListURLDetails(APIView):
-	def get(self, request):
-		url = request.query_params.get('url', None)
-		get_version = request.query_params.get('getversion', None)
-		verbose = request.query_params.get('verbose', None)
+    def get(self, request):
+        url = request.query_params.get('url', None)
+        get_version = request.query_params.get('getversion', None)
+        verbose = request.query_params.get('verbose', None)
 
-		if url is not None and url != "":
-			data_list = findIfJQueryExists(url) 
-			
-			if get_version is not None and get_version == "yes":
-				if data_list[0]:
-					if data_list[1] == 1:
-						if data_list[2] is not None:
-							data = {"success" : True, "uses_jquery" : "yes", "version" : data_list[2]}
-						else:
-							data = {"success" : True, "uses_jquery" : "yes", "version" : "could not detect"}	
-					elif data_list[1] == 0:
-						data = {"success" : True, "uses_jquery" : "maybe", "version" : data_list[2]}
-					elif data_list[1] == -1:
-						data = {"success" : True, "uses_jquery" : "no", "version" : data_list[2]}	
-					return Response(data)
-				else:
-					data = {"success" : False, "uses_jquery" : "no", "version" : data_list[2]}
-					return Response(data)
+        if url is not None and url != "":
+            data_list = findIfJQueryExists(url) 
+            
+            if get_version is not None and get_version == "yes":
+                if data_list[0]:
+                    if data_list[1] == 1:
+                        if data_list[2] is not None:
+                            data = {"success" : True, "uses_jquery" : "yes", "version" : data_list[2]}
+                        else:
+                            data = {"success" : True, "uses_jquery" : "yes", "version" : "could not detect"}    
+                    elif data_list[1] == 0:
+                        data = {"success" : True, "uses_jquery" : "maybe", "version" : data_list[2]}
+                    elif data_list[1] == -1:
+                        data = {"success" : True, "uses_jquery" : "no", "version" : data_list[2]}   
+                    return Response(data)
+                else:
+                    data = {"success" : False, "uses_jquery" : "no", "version" : data_list[2]}
+                    return Response(data)
 
-			if verbose is not None and verbose == "yes":
-				if data_list[0]:
-					if data_list[1] == 1:
-						data = {"success" : True, "uses_jquery" : "yes", "found_in_line" : data_list[3]}
-					elif data_list[1] == 0:
-						data = {"success" : True, "uses_jquery" : "maybe", "found_in_line" : data_list[3]}
-					elif data_list[1] == -1:
-						data = {"success" : True, "uses_jquery" : "no", "found_in_line" : data_list[3]}	
-					return Response(data)
-				else:
-					data = {"success" : False, "uses_jquery" : "no", "found_in_line" : data_list[3]}
-					return Response(data)		
+            if verbose is not None and verbose == "yes":
+                if data_list[0]:
+                    if data_list[1] == 1:
+                        data = {"success" : True, "uses_jquery" : "yes", "found_in_line" : data_list[3]}
+                    elif data_list[1] == 0:
+                        data = {"success" : True, "uses_jquery" : "maybe", "found_in_line" : data_list[3]}
+                    elif data_list[1] == -1:
+                        data = {"success" : True, "uses_jquery" : "no", "found_in_line" : data_list[3]} 
+                    return Response(data)
+                else:
+                    data = {"success" : False, "uses_jquery" : "no", "found_in_line" : data_list[3]}
+                    return Response(data)       
 
-			if data_list[0]:
-				if data_list[1] == 1:
-					data = {"success" : True, "uses_jquery" : "yes"}
-				elif data_list[1] == 0:
-					data = {"success" : True, "uses_jquery" : "maybe"}
-				elif data_list[1] == -1:
-					data = {"success" : True, "uses_jquery" : "no"}	
-				return Response(data)
-			else:
-				data = {"success" : False, "uses_jquery" : "no"}
-				return Response(data)
+            if data_list[0]:
+                if data_list[1] == 1:
+                    data = {"success" : True, "uses_jquery" : "yes"}
+                elif data_list[1] == 0:
+                    data = {"success" : True, "uses_jquery" : "maybe"}
+                elif data_list[1] == -1:
+                    data = {"success" : True, "uses_jquery" : "no"} 
+                return Response(data)
+            else:
+                data = {"success" : False, "uses_jquery" : "no"}
+                return Response(data)
 
-		else:
-			return Response(status=status.HTTP_400_BAD_REQUEST)			 
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)          
