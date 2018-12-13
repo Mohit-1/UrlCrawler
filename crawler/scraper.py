0a69a98bd0ba395019a552fc19216e201bba68ec
Mohit Solanki <mosolank@cisco.com>
2018-12-14 01:47:56 +0530

    PEP-8 compliance

--- crawler/scraper.py
+++ crawler/scraper.py
@@ -10,10 +10,8 @@ def findIfJQueryExists(url):
 
     data_dict = {"success": True, "uses_jquery": "no",
                  "version": "could not detect", "found_in_line": None}
-
     try:
         page_response = requests.get(url, timeout=5)
-
     except (ConnectionError, MissingSchema):
         # Invalid URL or a URL missing the protocol are caught here
         data_dict["success"] = False
@@ -24,8 +22,11 @@ def findIfJQueryExists(url):
     pattern_official = re.compile("http[s]{0,1}://ajax\.googleapis\.com/ajax/libs/jquery/([0-9]\.[0-9]+\.[0-9]+)/jquery(\.min){0,1}\.js|http[s]{0,1}://code\.jquery\.com/jquery-([0-9]\.[0-9]+\.[0-9]+)(\.min){0,1}\.js")
     pattern_unofficial = re.compile("jquery(\.min){0,1}\.js")
 
+    abort_loop = False
     for script in soup.findAll('script', {'src': True}):
         # Only collect those script tags which have an 'src' attribute
+        if abort_loop:
+            break
 
         match = pattern_official.search(script['src'])
         if match:
@@ -33,17 +34,17 @@ def findIfJQueryExists(url):
             data_dict["version"] = match.group(1) if match.group(1) else match.group(3)
             # Tricky part! Replacing the '<' with '&lt;'
             data_dict["found_in_line"] = str(script).split("</script>")[0].replace('<', '&lt;')
-            break
+            abort_loop = True
 
         elif (script['src'] == "http://code.jquery.com/jquery-latest.min.js" or
               script['src'] == "https://code.jquery.com/jquery-latest.min.js"):
             data_dict["uses_jquery"] = "yes"
             data_dict["found_in_line"] = str(script).split("</script>")[0].replace('<', '&lt;')
-            break
+            abort_loop = True
 
         elif pattern_unofficial.search(script['src']):
             data_dict["uses_jquery"] = "maybe"
             data_dict["found_in_line"] = str(script).split("</script>")[0].replace('<', '&lt;')
-            break
+            abort_loop = True
 
     return data_dict
