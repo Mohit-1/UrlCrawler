95296124f4c027281d0b2fc520a57d1efd3f5f06
Mohit Solanki <mosolank@cisco.com>
2018-12-14 01:26:10 +0530

    Refactored findIfJQueryExists

--- crawler/scraper.py
+++ crawler/scraper.py
@@ -1,59 +1,49 @@
 from bs4 import BeautifulSoup
-import requests, re
+import requests
+import re
 from requests.exceptions import ConnectionError, MissingSchema
 
+
 def findIfJQueryExists(url):
-    """ This method makes a request to the received URL and 
+    """ This method makes a request to the received URL and
     checks if jQuery exists in the URL's HTML source"""
 
-    success, uses_jquery, version, found_in_line = True, -1, None, None
-    data_list = []
+    data_dict = {"success": True, "uses_jquery": "no",
+                 "version": "could not detect", "found_in_line": None}
 
-    #proxy = {"http": "103.16.70.20:32231"} --- I prefer using proxies while scraping
-    #page_response = requests.get(page_link, timeout=5, proxies=proxy) --- omitting here to reduce vectors for failure
     try:
         page_response = requests.get(url, timeout=5)
-    
-    except (ConnectionError, MissingSchema): #Invalid URL or a URL missing the protocol are caught here
-        success, uses_jquery, version, found_in_line = False, -1, None, None
-        data_list.append(success)
-        data_list.append(uses_jquery)
-        data_list.append(version)
-        data_list.append(found_in_line)     
 
-        return data_list            
+    except (ConnectionError, MissingSchema):
+        # Invalid URL or a URL missing the protocol are caught here
+        data_dict["success"] = False
+        return data_dict
 
     soup = BeautifulSoup(page_response.content, "html.parser")
 
-    for script in soup.findAll('script', {'src': True}):    # Only collect those script tags which have an 'src' attribute
+    pattern_official = re.compile("http[s]{0,1}://ajax\.googleapis\.com/ajax/libs/jquery/([0-9]\.[0-9]+\.[0-9]+)/jquery(\.min){0,1}\.js|http[s]{0,1}://code\.jquery\.com/jquery-([0-9]\.[0-9]+\.[0-9]+)(\.min){0,1}\.js")
+    pattern_unofficial = re.compile("jquery(\.min){0,1}\.js")
 
-        match = re.search("http[s]{0,1}://ajax\.googleapis\.com/ajax/libs/jquery/([0-9]\.[0-9]+\.[0-9]+)/jquery(\.min){0,1}\.js", script['src'])
+    for script in soup.findAll('script', {'src': True}):
+        # Only collect those script tags which have an 'src' attribute
+
+        match = pattern_official.search(script['src'])
         if match:
-            uses_jquery = 1
-            version = match.group(1)
-            found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')  # Tricky part! Replacing the '<' with '&lt;'
+            data_dict["uses_jquery"] = "yes"
+            data_dict["version"] = match.group(1) if match.group(1) else match.group(3)
+            # Tricky part! Replacing the '<' with '&lt;'
+            data_dict["found_in_line"] = str(script).split("</script>")[0].replace('<', '&lt;')
             break
 
-        match = re.search("http[s]{0,1}://code\.jquery\.com/jquery-([0-9]\.[0-9]+\.[0-9]+)(\.min){0,1}\.js", script['src'])
-        if match:
-            uses_jquery = 1
-            version = match.group(1)
-            found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')  # Tricky part! Replacing the '<' with '&lt;'
+        elif (script['src'] == "http://code.jquery.com/jquery-latest.min.js" or
+              script['src'] == "https://code.jquery.com/jquery-latest.min.js"):
+            data_dict["uses_jquery"] = "yes"
+            data_dict["found_in_line"] = str(script).split("</script>")[0].replace('<', '&lt;')
             break
 
-        elif script['src'] == "http://code.jquery.com/jquery-latest.min.js" or script['src'] == "https://code.jquery.com/jquery-latest.min.js":
-            uses_jquery = 1
-            found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')
+        elif pattern_unofficial.search(script['src']):
+            data_dict["uses_jquery"] = "maybe"
+            data_dict["found_in_line"] = str(script).split("</script>")[0].replace('<', '&lt;')
             break
 
-        elif re.search("jquery(\.min){0,1}\.js", script['src']):
-            uses_jquery = 0
-            found_in_line = str(script).split("</script>")[0].replace('<', '&lt;')
-            break
-
-    data_list.append(success)
-    data_list.append(uses_jquery)
-    data_list.append(version)
-    data_list.append(found_in_line)     
-
-    return data_list            
+    return data_dict
